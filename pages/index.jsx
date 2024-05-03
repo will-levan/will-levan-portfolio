58a98a8d373df3d0b2ba1fe2ab8ee3bc8801f779
williamlevan <wlevan@stlcitysc.com>
2024-05-03 12:26:01 -0500

    test button

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Home = () => {
 
+    const clickTest = () => {
+        console.log("click test");
+    }
+
     return (
         <div>
             <h1>Home</h1>
+            <button onClick={clickTest}>TEST</button>
         </div>
     )
 }
